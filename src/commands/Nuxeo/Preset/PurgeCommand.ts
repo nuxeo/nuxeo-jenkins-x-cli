0ea28a5f5094edd1d2c1acbce9afad409b1a6329
Arnaud Kervern <akervern@nuxeo.com>
2019-02-14 17:41:16 +0100

    NXBT-2736: Fill preset remove cmd

--- src/commands/Nuxeo/Preset/PurgeCommand.ts
+++ src/commands/Nuxeo/Preset/PurgeCommand.ts
@@ -1,7 +1,5 @@
-import debug from 'debug';
 import { Arguments, CommandModule } from 'yargs';
-
-const log: debug.IDebugger = debug('command:nuxeo:preset:purge');
+import { ProcessSpawner } from '../../../lib/ProcessSpawner';
 
 /**
  * Nuxeo Preset Purge Command - Purge previously installed Nuxeo Preset
@@ -11,7 +9,11 @@ export class PurgeCommand implements CommandModule {
 
   public describe: string = 'purge a preset';
 
-  public handler = (args: Arguments): void => {
-    log(args);
+  public handler = async (args: Arguments): Promise<string> => {
+    return ProcessSpawner.createSub(args)
+      .arg('helm')
+      .arg('cleanup')
+      .arg('--name').arg(args.name)
+      .execWithSpinner();
   }
 }
