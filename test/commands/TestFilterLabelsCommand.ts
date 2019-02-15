de0ce6dc4b51b30924c5139268438b2af814c038
Gildas Lefevre <glefevre@nuxeo.com>
2019-02-15 14:25:01 -0500

    NXBT-2737: Write the filtered list in the stdout of the process

--- test/commands/TestFilterLabelsCommand.ts
+++ test/commands/TestFilterLabelsCommand.ts
@@ -1,6 +1,5 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { Arguments } from 'yargs';
 
 import { FilterLabels } from '../../src/commands/PullRequest/FilterLabels';
 
@@ -8,37 +7,22 @@ describe('Filter-Labels Command', () => {
   const cmd = new FilterLabels();
 
   it('filters labels for test', () => {
-    const args: Arguments = {
-      _: ['pr', 'filter-labels'],
-      mode: 'test',
-      labels: ['test/mongodb', 'preview/es', 'testes'],
-      '$0': 'src/index.ts',
-    };
-    const content: string[] = cmd.handler(args);
+    const labels = ['test/mongodb', 'preview/es', 'testes'];
+    const content: string[] = cmd['filterLabels'](labels, 'test');
 
     expect(content).to.eql(['mongodb']);
   })
 
   it('filters labels for preview', () => {
-    const args: Arguments = {
-      _: ['pr', 'filter-labels'],
-      mode: 'preview',
-      labels: ['test/mongodb', 'preview/es', 'testes', 'preview/mongodb'],
-      '$0': 'src/index.ts',
-    };
-    const content: string[] = cmd.handler(args);
+    const labels = ['test/mongodb', 'preview/es', 'testes', 'preview/mongodb'];
+    const content: string[] = cmd['filterLabels'](labels, 'preview');
 
     expect(content).to.eql(['es', 'mongodb']);
   })
 
   it('filters all labels if nothing matches', () => {
-    const args: Arguments = {
-      _: ['pr', 'filter-labels'],
-      mode: 'unittest',
-      labels: ['test/mongodb', 'preview/es', 'testes', 'preview/mongodb'],
-      '$0': 'src/index.ts',
-    };
-    const content: string[] = cmd.handler(args);
+    const labels = ['test/mongodb', 'preview/es', 'testes', 'preview/mongodb'];
+    const content: string[] = cmd['filterLabels'](labels, 'unit-test');
 
     expect(content).length(0);
   })
